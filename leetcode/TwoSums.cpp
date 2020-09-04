2d2b3a9f3133e15dbdf2be1ad7e27ae7dfa69ea5
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-09-04 20:47:40 +0200

    Solve TwoSums in one pass

--- leetcode/TwoSums.cpp
+++ leetcode/TwoSums.cpp
@@ -3,29 +3,26 @@
 using namespace std;
 
 class Solution {
-public:
-  vector<int> twoSum(vector<int> &nums, int target) {
-    unordered_map<int, int> index_by_num;
-    for (int i = 0; i < nums.size(); i++) {
-      if (index_by_num.count(nums[i]) != 0) {
-        if (nums[i] * 2 == target) {
-          vector<int> solution = {index_by_num[nums[i]], i};
-          return solution;
+  public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> index_by_num;
+        for (int i = 0; i < nums.size(); i++) {
+            int pair_index = target - nums[i];
+            if (index_by_num.count(pair_index) != 0 && index_by_num[pair_index] != i) {
+                vector<int> solution = {index_by_num[pair_index], i};
+                return solution;
+            }
+
+            index_by_num[nums[i]] = i;
         }
-      }
 
-      index_by_num[nums[i]] = i;
+        int i = nums.size() - 1;
+        int pair_index = target - nums[i];
+        if (index_by_num.count(pair_index) != 0 && index_by_num[pair_index] != i) {
+            vector<int> solution = {index_by_num[pair_index], i};
+            return solution;
+        }
+
+        return {};
     }
-
-    for (int i = 0; i < nums.size(); i++) {
-      int pair_index = target - nums[i];
-      if (index_by_num.count(pair_index) != 0 &&
-          index_by_num[pair_index] != i) {
-        vector<int> solution = {index_by_num[pair_index], i};
-        return solution;
-      }
-    }
-
-    return {};
-  }
 };
